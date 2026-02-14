22196562ce6c9033c7cd68d6a8d315a8061ccb05
TarsoRondon <rondontarso2009@gmail.com>
2026-02-14 01:07:52 -0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+﻿require('dotenv').config();
+
+const app = require('./src/app');
+const { startDiarySyncWorker } = require('./src/services/diarySyncService');
+const { logInfo, logError } = require('./src/utils/logger');
+
+const port = Number(process.env.PORT || 3000);
+
+const server = app.listen(port, () => {
+  logInfo('server_started', { port });
+});
+
+startDiarySyncWorker();
+
+process.on('unhandledRejection', (reason) => {
+  logError('unhandled_rejection', { reason: reason?.message || String(reason) });
+});
+
+process.on('uncaughtException', (error) => {
+  logError('uncaught_exception', { error: error.message, stack: error.stack });
+});
+
+module.exports = server;
